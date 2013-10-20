737e7c50e01cbe638fd10995500fc7cf800bdadf
Steve Kondik <shade@chemlab.org>
2013-10-19 19:49:20 -0700

    recovery: Get a proper shell environment in recovery
     * ADB support
     * Toybox applets
     * mksh
     * Set time
    
    Change-Id: I80b0e2aa5eb7142eaa9f157709f4e029077d8dfa

--- common.h
+++ common.h
@@ -26,6 +26,9 @@
 // device-specific recovery libraries. We static assert the value consistency in recovery.cpp.
 static constexpr int kRecoveryApiVersion = 3;
 
+// Minimum valid seconds since epoch (2019-01-01 00:00:00)
+#define TV_MIN 1546300800
+
 class RecoveryUI;
 
 extern RecoveryUI* ui;
