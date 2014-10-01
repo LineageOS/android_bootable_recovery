7369878ba0949b7889f378a1c5eeb3040f533756
Tom Marshall <tdm@cyngn.com>
2014-10-01 14:49:05 -0700

    recovery: Use pathmap for recovery
    Change-Id: I3da83bb70dbbb24016d9b231443daeb0b9c2c86a

--- otafault/Android.mk
+++ otafault/Android.mk
@@ -33,7 +33,7 @@ LOCAL_SRC_FILES := config.cpp ota_io.cpp
 LOCAL_MODULE_TAGS := eng
 LOCAL_MODULE := libotafault
 LOCAL_CLANG := true
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_WHOLE_STATIC_LIBRARIES := $(otafault_static_libs)
 
@@ -48,7 +48,7 @@ LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE := otafault_test
 LOCAL_STATIC_LIBRARIES := $(otafault_static_libs)
 LOCAL_CFLAGS := -Werror
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 include $(BUILD_EXECUTABLE)
--- tests/Android.mk
+++ tests/Android.mk
@@ -41,7 +41,7 @@ LOCAL_SRC_FILES := \
     unit/zip_test.cpp \
     unit/ziputil_test.cpp
 
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SHARED_LIBRARIES := liblog
 include $(BUILD_NATIVE_TEST)
 
@@ -58,7 +58,7 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libpng
 
-resource_files := $(call find-files-in-subdirs, bootable/recovery, \
+resource_files := $(call find-files-in-subdirs, $(call project-path-for,recovery), \
     "*_text.png", \
     res-mdpi/images \
     res-hdpi/images \
@@ -73,7 +73,7 @@ GEN := $(addprefix $(testimage_out_path)/, $(resource_files))
 
 $(GEN): PRIVATE_PATH := $(LOCAL_PATH)
 $(GEN): PRIVATE_CUSTOM_TOOL = cp $< $@
-$(GEN): $(testimage_out_path)/% : bootable/recovery/%
+$(GEN): $(testimage_out_path)/% : $(call project-path-for,recovery)/%
 	$(transform-generated-source)
 LOCAL_GENERATED_SOURCES += $(GEN)
 
@@ -99,7 +99,7 @@ endif
 
 LOCAL_MODULE := recovery_component_test
 LOCAL_COMPATIBILITY_SUITE := device-tests
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SRC_FILES := \
     component/applypatch_test.cpp \
     component/bootloader_message_test.cpp \
@@ -195,7 +195,7 @@ include $(CLEAR_VARS)
 LOCAL_CFLAGS := -Werror
 LOCAL_MODULE := recovery_host_test
 LOCAL_MODULE_HOST_OS := linux
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SRC_FILES := \
     component/imgdiff_test.cpp
 LOCAL_STATIC_LIBRARIES := \
