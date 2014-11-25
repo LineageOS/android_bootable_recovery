1ec535f18ca707bc447ac1cec237fc2a3dcf6ac1
Tom Marshall <tdm@cyngn.com>
2014-11-24 16:02:04 -0800

    recovery: Awakening of MiniVold
    A minimal vold client for recovery.
    
    Change-Id: Id25d955dc1861a910e5f5fc27d9a19e245d66833

--- ui.h
+++ ui.h
@@ -23,6 +23,8 @@
 
 #include <string>
 
+#include "voldclient.h"
+
 #define MAX_NR_VKEYS	8
 
 /*
@@ -153,6 +155,9 @@ class RecoveryUI {
   // Ends menu mode, resetting the text overlay so that ui_print() statements will be displayed.
   virtual void EndMenu() = 0;
 
+  // Notify of volume state change
+  void onVolumeChanged() { volumes_changed_ = 1; }
+
  protected:
   void EnqueueKey(int key_code);
 
@@ -170,6 +175,7 @@ class RecoveryUI {
   bool touch_screen_allowed_;
 
  private:
+  bool volumes_changed_;
   // The sensitivity when detecting a swipe.
   const int kTouchLowThreshold;
   const int kTouchHighThreshold;
@@ -223,6 +229,8 @@ class RecoveryUI {
   void ProcessKey(int key_code, int updown);
 
   bool IsUsbConnected();
+
+  bool VolumesChanged();
 
   static void* time_key_helper(void* cookie);
   void time_key(int key_code, int count);
