cd56946b9f5a94fd88b716a98bd19db8eb05c6cf
David Ng <dave@codeaurora.org>
2015-08-24 18:56:25 -0700

    recovery: Allow device-specific reboot reason
    Allow overriding of reboot reason.
    
    Change-Id: I73d9debaed8224bf6877008b15aa95c6c404c624

--- recovery.cpp
+++ recovery.cpp
@@ -1896,8 +1896,10 @@ error:
             break;
 
         default:
+            char reason[PROPERTY_VALUE_MAX];
+            snprintf(reason, PROPERTY_VALUE_MAX, "reboot,%s", device->GetRebootReason());
             ui->Print("Rebooting...\n");
-            property_set(ANDROID_RB_PROPERTY, "reboot,");
+            property_set(ANDROID_RB_PROPERTY, reason);
             break;
     }
     while (true) {
