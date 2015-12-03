1ceb55bed877fa019bc2212a97e8fd4a8fccb67e
Michael Bestas <mikeioannina@gmail.com>
2015-12-03 20:56:42 +0200

    recovery: Remove "Supported API" message
    Change-Id: Iac2153403ee66c8544c7d809f7b037aedeaf9d21

--- recovery.cpp
+++ recovery.cpp
@@ -1779,8 +1779,6 @@ int main(int argc, char **argv) {
   property_list(print_property, nullptr);
   printf("\n");
 
-  ui->Print("Supported API: %d\n", kRecoveryApiVersion);
-
   int status = INSTALL_SUCCESS;
 
   if (update_package != nullptr) {
