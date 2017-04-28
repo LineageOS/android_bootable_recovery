2a7b417a84f01fa802641ce4023b9ffc933a3ca7
Michael Bestas <mkbestas@lineageos.org>
2017-04-28 22:46:40 +0300

    recovery: Complete project pathmap usage
    * Use project pathmap for LOCAL_C_INCLUDES
    * Use project pathmap for scripts
    
    Change-Id: I3da83bb70dbbb24016d9b231443daeb0b9c2c86a

--- minadbd/Android.mk
+++ minadbd/Android.mk
@@ -18,7 +18,7 @@ LOCAL_SRC_FILES := \
 LOCAL_MODULE := libminadbd
 LOCAL_CFLAGS := $(minadbd_cflags)
 LOCAL_CONLY_FLAGS := -Wimplicit-function-declaration
-LOCAL_C_INCLUDES := bootable/recovery system/core/adb
+LOCAL_C_INCLUDES := $(call project-path-for,recovery) system/core/adb
 LOCAL_WHOLE_STATIC_LIBRARIES := libadbd
 LOCAL_STATIC_LIBRARIES := libcrypto libbase
 
