888edfbab600d6998b494d6e83804a001960d7c0
Tom Marshall <tdm.code@gmail.com>
2017-08-24 12:57:27 +0000

    recovery: ui: Minor cleanup for touch code
     * Better naming for some touch vars and funcs
    
     * Introduce Point class
    
    Change-Id: Idfcab54ac356face52efd69fdfdc0a6f4633a3f3

--- minui/include/minui/minui.h
+++ minui/include/minui/minui.h
@@ -155,8 +155,8 @@ using ev_set_key_callback = std::function<int(int code, int value)>;
 int ev_init(ev_callback input_cb, bool allow_touch_inputs = false);
 void ev_exit();
 int ev_add_fd(android::base::unique_fd&& fd, ev_callback cb);
-void ev_iterate_available_keys(const std::function<void(int)>& f);
-void ev_iterate_touch_inputs(const std::function<void(int)>& action);
+void ev_iterate_available_keys(const std::function<void(int)>& key_detected);
+void ev_iterate_touch_inputs(const std::function<void(int)>& key_detected);
 int ev_sync_key_state(const ev_set_key_callback& set_key_cb);
 
 // 'timeout' has the same semantics as poll(2).
