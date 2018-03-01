6d11cd0ab34ac14df54546a5c48c548d0dff84b8
Tom Marshall <tdm.code@gmail.com>
2018-03-01 23:51:51 +0100

    recovery: Fix redraws, flickering, and animation
    Rewrite screen drawing code to behave as follows:
    
     * draw_screen_locked has exactly two code paths: menu and non-menu.
        Non-menu code path draws background, foreground, and text.
     * Define screen background as the stage marker.
     * Define screen foreground as either the animation and progress bar
       or the icon and text.
     * Restore redraw optimizations in update_progress_locked.
     * Get rid of spurious screen updates.  The screen is only updated
        in StartMenu, SelectMenu, Redraw, and the progress thread.
     * Various changes and fixes to support the above.
    
    Additionally:
    
     * Show error text on screen after failed install.
     * Rewrite graphics test code to better exercise the above.
    
    Change-Id: I598bb11d5eef28275a6501cd44d514afe9939885

--- recovery.cpp
+++ recovery.cpp
@@ -1093,36 +1093,31 @@ static int choose_recovery_file(Device* device) {
 }
 
 static void run_graphics_test() {
-  // Switch to graphics screen.
-  ui->ShowText(false);
-
-  ui->SetProgressType(RecoveryUI::INDETERMINATE);
-  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
-  sleep(1);
-
   ui->SetBackground(RecoveryUI::ERROR);
+  ui->Redraw();
   sleep(1);
 
-  ui->SetBackground(RecoveryUI::NO_COMMAND);
+  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
+  ui->Redraw();
   sleep(1);
 
   ui->SetBackground(RecoveryUI::ERASING);
+  ui->Redraw();
   sleep(1);
 
-  // Calling SetBackground() after SetStage() to trigger a redraw.
   ui->SetStage(1, 3);
-  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
+  ui->Redraw();
   sleep(1);
   ui->SetStage(2, 3);
-  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
+  ui->Redraw();
   sleep(1);
   ui->SetStage(3, 3);
-  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
+  ui->Redraw();
   sleep(1);
 
   ui->SetStage(-1, -1);
-  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
 
+  ui->SetBackground(RecoveryUI::INSTALLING_UPDATE);
   ui->SetProgressType(RecoveryUI::DETERMINATE);
   ui->ShowProgress(1.0, 10.0);
   float fraction = 0.0;
@@ -1131,8 +1126,6 @@ static void run_graphics_test() {
     ui->SetProgress(fraction);
     usleep(100000);
   }
-
-  ui->ShowText(true);
 }
 
 static int apply_from_storage(Device* device, const std::string& id, bool* wipe_cache) {
@@ -1306,9 +1299,13 @@ static Device::BuiltinAction prompt_and_wait(Device* device, int status) {
             }
 
             if (status != INSTALL_SUCCESS) {
+              ui->SetProgressType(RecoveryUI::EMPTY);
               ui->SetBackground(RecoveryUI::ERROR);
               ui->Print("Installation aborted.\n");
+              ui->Redraw();
               copy_logs();
+              ui->FlushKeys();
+              ui->WaitInputEvent();
             } else if (!ui->IsTextVisible()) {
               return Device::NO_ACTION;  // reboot if logs aren't visible
             } else {
@@ -1834,6 +1831,7 @@ int main(int argc, char **argv) {
     if (status == INSTALL_ERROR || status == INSTALL_CORRUPT) {
         ui->SetBackground(RecoveryUI::ERROR);
         if (!ui->IsTextVisible()) {
+            ui->Redraw();
             sleep(5);
         }
     }
