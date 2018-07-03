25daa371514c79a1456c0c1e44c7d61276317b91
Simon Shields <simon@lineageos.org>
2018-07-04 01:56:56 +1000

    minui: drm: ARGB8888 support
    Change-Id: Id00ef1293bd3f8d81f0d82560e02ce0cddb0bb80

--- minui/graphics_drm.cpp
+++ minui/graphics_drm.cpp
@@ -99,6 +99,7 @@ static int drm_format_to_bpp(uint32_t format) {
     case DRM_FORMAT_BGRX8888:
     case DRM_FORMAT_XBGR8888:
     case DRM_FORMAT_XRGB8888:
+    case DRM_FORMAT_ARGB8888:
       return 32;
     case DRM_FORMAT_RGB565:
       return 16;
