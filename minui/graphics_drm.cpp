815c4bfcf899d2efbbc7e4b75129ba6d502db6fd
David Ng <dave@codeaurora.org>
2018-09-13 18:45:52 -0700

    minui: Handle NULL return conditions and potential beyond bound access
    Fix NULL return handling in minui for DRM scenario.
    
    CRs-Fixed: 2315166
    Change-Id: Ifa9f5d055a0cbcfadd505edc1a5f13b97605c92d

--- minui/graphics_drm.cpp
+++ minui/graphics_drm.cpp
@@ -653,7 +653,7 @@ GRSurface* MinuiBackendDrm::Init() {
 
   /* Get possible plane_ids */
   drmModePlaneRes *plane_options = drmModeGetPlaneResources(drm_fd);
-  if (!plane_options)
+  if (!plane_options || !plane_options->planes || (plane_options->count_planes < NUM_PLANES))
     return NULL;
 
   drmSetClientCap(drm_fd, DRM_CLIENT_CAP_UNIVERSAL_PLANES, 1);
@@ -663,10 +663,13 @@ GRSurface* MinuiBackendDrm::Init() {
   crtc_res.props = drmModeObjectGetProperties(drm_fd,
                       drm[DRM_MAIN].monitor_crtc->crtc_id,
                       DRM_MODE_OBJECT_CRTC);
+  if (!crtc_res.props)
+    return NULL;
+
   crtc_res.props_info = static_cast<drmModePropertyRes **>
                            (calloc(crtc_res.props->count_props,
                            sizeof(crtc_res.props_info)));
-  if (!crtc_res.props || !crtc_res.props_info)
+  if (!crtc_res.props_info)
     return NULL;
   else
     for (int j = 0; j < (int)crtc_res.props->count_props; ++j)
@@ -677,10 +680,13 @@ GRSurface* MinuiBackendDrm::Init() {
   conn_res.props = drmModeObjectGetProperties(drm_fd,
                      drm[DRM_MAIN].monitor_connector->connector_id,
                      DRM_MODE_OBJECT_CONNECTOR);
+  if (!conn_res.props)
+    return NULL;
+
   conn_res.props_info = static_cast<drmModePropertyRes **>
                          (calloc(conn_res.props->count_props,
                          sizeof(conn_res.props_info)));
-  if(!conn_res.props || !conn_res.props_info)
+  if (!conn_res.props_info)
     return NULL;
   else
     for (int j = 0; j < (int)conn_res.props->count_props; ++j)
