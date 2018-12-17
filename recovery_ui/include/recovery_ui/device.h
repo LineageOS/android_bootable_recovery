92a58d332c3ed18eaee2651f206d5d2ead98b02d
Tom Marshall <tdm.code@gmail.com>
2018-12-17 15:57:44 -0800

    recovery: Provide sideload cancellation
    We can't use InterruptWaitKey() as it hangs recovery
    when called from the minadbd listener thread, so provide
    our own 'CancelWaitKey' implementation.
    [forkbomb: rework for Q]
    
    Squashed:
      recovery: Move sideload (non)cancellation to its own key
    
       * We use KEY_REFRESH for actually refreshing menus, which is
         useful when handling hotpluggable storage.
         We don't want that to interact with the sideload menu.
    
      Change-Id: Id7f5c06385e613648b3f727fc5e91eb3206406cf
    
    Change-Id: I13f0c9ae5444652a2141442ef24258679a78d320

--- recovery_ui/include/recovery_ui/device.h
+++ recovery_ui/include/recovery_ui/device.h
@@ -38,6 +38,8 @@ class Device {
   static constexpr const int kInvokeItem = -4;
   static constexpr const int kGoBack = -5;
   static constexpr const int kGoHome = -6;
+  static constexpr const int kRefresh = -7;
+  static constexpr const int kDoSideload = -8;
 
   // ENTER vs REBOOT: The latter will trigger a reboot that goes through bootloader, which allows
   // using a new bootloader / recovery image if applicable. For example, REBOOT_RESCUE goes from
