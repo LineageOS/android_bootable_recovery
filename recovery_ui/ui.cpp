659bf955b4ae2104db5a3eba979da3b1ac7d04d0
Tom Marshall <tdm.code@gmail.com>
2019-01-04 14:37:31 -0800

    recovery: Implement a volume manager
    This is a copy of the pre-binderized vold which has been converted to
    use direct calls instead of sockets and stripped down to only what is
    needed to support recovery.
    
    Includes:
      * recovery: volmgr: remove unused IsSupported
    
        Change-Id: If8206658fdfb6108221806c09c99bf0a30f4a586
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
      * recovery: volmgr: remove filesystem checks
    
        Those checks are not strictly necessary and we are
        not building fsck tools for recovery for now.
    
        Remove those checks so volmgr can be useful.
    
        Change-Id: I87756c61b933b6cdccd281c6276b686fbd36019f
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
      * recovery: fixup `EmulatedVolume creating`
    
        Avoid dangling pointer. Instead of pointing to FstabEntry create copy.
    
        Change-Id: I57f76006db09a6add2c173f43175f0f6b848d87b
    
      * recovery: fix volmgr cleaning up
    
        Don't reset pointer to netlink manager
        Delete disks in stop() rather then in ~VolumeManager
        Call destroy() before deleting disks cause delete expects the
          disk to be destroyed
        Clear the lists or we would read garbage data on the next scan
    
        Change-Id: Idadfa1f33b7cb5f2f3c780848a99344a6608420e
    
      * recovery: handle interrupts in apply update menu
    
        Change-Id: I1f78f9196634353b77986545332d7d52a5f0c161
    
    Change-Id: Ic82d929e052b5ba70ecf7b475e0a223d77d9687e

--- recovery_ui/ui.cpp
+++ recovery_ui/ui.cpp
@@ -36,6 +36,7 @@
 #include <android-base/parseint.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
+#include <volume_manager/VolumeManager.h>
 
 #include "minui/minui.h"
 #include "otautil/sysutil.h"
@@ -466,6 +467,7 @@ void RecoveryUI::ProcessKey(int key_code, int updown) {
 
       case RecoveryUI::REBOOT:
         if (reboot_enabled) {
+          android::volmgr::VolumeManager::Instance()->unmountAll();
           Reboot("userrequested,recovery,ui");
         }
         break;
---
+++ volume_manager/fs/Vfat.cpp
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <linux/fs.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <linux/kdev_t.h>
+
+#define LOG_TAG "Vold"
+
+#include <android-base/logging.h>
+#include <android-base/stringprintf.h>
+#include <cutils/log.h>
+#include <cutils/properties.h>
+#include <selinux/selinux.h>
+
+#include <logwrap/logwrap.h>
+
+#include "Utils.h"
+#include "Vfat.h"
+
+using android::base::StringPrintf;
+
+namespace android {
+namespace volmgr {
+namespace vfat {
+
+status_t Mount(const std::string& source, const std::string& target, bool ro, bool remount,
+               bool executable, int ownerUid, int ownerGid, int permMask, bool createLost) {
+    int rc;
+    unsigned long flags;
+    char mountData[255];
+
+    const char* c_source = source.c_str();
+    const char* c_target = target.c_str();
+
+    flags = MS_NODEV | MS_NOSUID | MS_DIRSYNC | MS_NOATIME;
+
+    flags |= (executable ? 0 : MS_NOEXEC);
+    flags |= (ro ? MS_RDONLY : 0);
+    flags |= (remount ? MS_REMOUNT : 0);
+
+    snprintf(mountData, sizeof(mountData), "utf8,uid=%d,gid=%d,fmask=%o,dmask=%o,shortname=mixed",
+             ownerUid, ownerGid, permMask, permMask);
+
+    rc = mount(c_source, c_target, "vfat", flags, mountData);
+
+    if (rc && errno == EROFS) {
+        SLOGE("%s appears to be a read only filesystem - retrying mount RO", c_source);
+        flags |= MS_RDONLY;
+        rc = mount(c_source, c_target, "vfat", flags, mountData);
+    }
+
+    if (rc == 0 && createLost) {
+        char* lost_path;
+        asprintf(&lost_path, "%s/LOST.DIR", c_target);
+        if (access(lost_path, F_OK)) {
+            /*
+             * Create a LOST.DIR in the root so we have somewhere to put
+             * lost cluster chains (fsck_msdos doesn't currently do this)
+             */
+            if (mkdir(lost_path, 0755)) {
+                SLOGE("Unable to create LOST.DIR (%s)", strerror(errno));
+            }
+        }
+        free(lost_path);
+    }
+
+    return rc;
+}
+
+}  // namespace vfat
+}  // namespace volmgr
+}  // namespace android
