b51aa681b61c17e2b1d1dfcea8defc3a34a8ddef
Tom Marshall <tdm.code@gmail.com>
2019-01-04 14:37:31 -0800

    recovery: Implement a volume manager
    This is a copy of the pre-binderized vold which has been converted to
    use direct calls instead of sockets and stripped down to only what is
    needed to support recovery.
    
    Change-Id: Ic82d929e052b5ba70ecf7b475e0a223d77d9687e

--- recovery_ui/screen_ui.cpp
+++ recovery_ui/screen_ui.cpp
@@ -1366,7 +1366,8 @@ int ScreenRecoveryUI::ScrollMenu(int updown) {
 }
 
 size_t ScreenRecoveryUI::ShowMenu(std::unique_ptr<Menu>&& menu, bool menu_only,
-                                  const std::function<int(int, bool)>& key_handler) {
+                                  const std::function<int(int, bool)>& key_handler,
+                                  bool refresbable) {
   // Throw away keys pressed previously, so user doesn't accidentally trigger menu items.
   FlushKeys();
 
@@ -1438,7 +1439,9 @@ size_t ScreenRecoveryUI::ShowMenu(std::unique_ptr<Menu>&& menu, bool menu_only,
         case Device::kNoAction:
           break;
         case Device::kRefresh:
-          chosen_item = Device::kRefresh;
+          if (refresbable) {
+            chosen_item = Device::kRefresh;
+          }
           break;
         case Device::kGoBack:
           chosen_item = Device::kGoBack;
@@ -1466,13 +1469,15 @@ size_t ScreenRecoveryUI::ShowMenu(std::unique_ptr<Menu>&& menu, bool menu_only,
 size_t ScreenRecoveryUI::ShowMenu(const std::vector<std::string>& headers,
                                   const std::vector<std::string>& items, size_t initial_selection,
                                   bool menu_only,
-                                  const std::function<int(int, bool)>& key_handler) {
+                                  const std::function<int(int, bool)>& key_handler,
+                                  bool refresbable) {
   auto menu = CreateMenu(headers, items, initial_selection);
   if (menu == nullptr) {
     return initial_selection;
   }
 
-  return ShowMenu(CreateMenu(headers, items, initial_selection), menu_only, key_handler);
+  return ShowMenu(CreateMenu(headers, items, initial_selection), menu_only, key_handler,
+                  refresbable);
 }
 
 size_t ScreenRecoveryUI::ShowPromptWipeDataMenu(const std::vector<std::string>& backup_headers,
--- volclient.cpp
+++ volclient.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,20 @@
  * limitations under the License.
  */
 
-#pragma once
+#include "volclient.h"
+#include "volume_manager/ResponseCode.h"
 
-#include "recovery_ui/device.h"
-#include "recovery_ui/ui.h"
-#include <volume_manager/VolumeManager.h>
+void VolumeClient::handleEvent(int code, const std::vector<std::string>& argv) {
+  // This client is only interested in volume addition/deletion
+  if (code != ResponseCode::VolumeDestroyed &&
+      code != ResponseCode::DiskScanned)
+    return;
 
-using android::volmgr::VolumeInfo;
+  printf("VolumeClient::handleEvent: code=%d, argv=<", code);
+  for (auto& arg : argv) {
+    printf("%s,", arg.c_str());
+  }
+  printf(">\n");
 
-int ApplyFromStorage(Device* device, VolumeInfo& vi, RecoveryUI* ui);
+  mDevice->handleVolumeChanged();
+}
