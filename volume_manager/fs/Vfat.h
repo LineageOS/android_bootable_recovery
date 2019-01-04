bead261398f56ffc87757f14c62eea3a8fa67e27
Tom Marshall <tdm.code@gmail.com>
2019-01-04 14:37:31 -0800

    recovery: Implement a volume manager
    This is a copy of the pre-binderized vold which has been converted to
    use direct calls instead of sockets and stripped down to only what is
    needed to support recovery.
    
    Change-Id: Ic82d929e052b5ba70ecf7b475e0a223d77d9687e

--- volume_manager/fs/Vfat.h
+++ volume_manager/fs/Vfat.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +15,25 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef VOLMGR_VFAT_H
+#define VOLMGR_VFAT_H
 
-#include "recovery_ui/device.h"
-#include "recovery_ui/ui.h"
-#include <volume_manager/VolumeManager.h>
+#include <utils/Errors.h>
 
-using android::volmgr::VolumeInfo;
+#include <string>
 
-int ApplyFromStorage(Device* device, VolumeInfo& vi, RecoveryUI* ui);
+namespace android {
+namespace volmgr {
+namespace vfat {
+
+bool IsSupported();
+
+status_t Check(const std::string& source);
+status_t Mount(const std::string& source, const std::string& target, bool ro, bool remount,
+               bool executable, int ownerUid, int ownerGid, int permMask, bool createLost);
+
+}  // namespace vfat
+}  // namespace volmgr
+}  // namespace android
+
+#endif
--- volume_manager/sehandle.h
+++ volume_manager/sehandle.h
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2019 The Android Open Source Project
+ * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +15,11 @@
  * limitations under the License.
  */
 
-#pragma once
+#ifndef _SEHANDLE_H
+#define _SEHANDLE_H
 
-#include "recovery_ui/device.h"
-#include "recovery_ui/ui.h"
-#include <volume_manager/VolumeManager.h>
+#include <selinux/android.h>
 
-using android::volmgr::VolumeInfo;
+extern struct selabel_handle* sehandle;
 
-int ApplyFromStorage(Device* device, VolumeInfo& vi, RecoveryUI* ui);
+#endif
