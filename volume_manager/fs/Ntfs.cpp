6bed5ff26d8d85351d0e1d57efd95198420e2975
Tom Marshall <tdm.code@gmail.com>
2019-01-04 14:37:31 -0800

    recovery: Implement a volume manager
    This is a copy of the pre-binderized vold which has been converted to
    use direct calls instead of sockets and stripped down to only what is
    needed to support recovery.
    
    Includes:
      * recovery: volmgr: remove unused IsSupported
    
        Change-Id: If8206658fdfb6108221806c09c99bf0a30f4a586
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
      * recovery: volmgr: remove filesystem checks
    
        Those checks are not strictly necessary and we are
        not building fsck tools for recovery for now.
    
        Remove those checks so volmgr can be useful.
    
        Change-Id: I87756c61b933b6cdccd281c6276b686fbd36019f
        Signed-off-by: Jesse Chan <jc@lineageos.org>
    
      * recovery: fixup `EmulatedVolume creating`
    
        Avoid dangling pointer. Instead of pointing to FstabEntry create copy.
    
        Change-Id: I57f76006db09a6add2c173f43175f0f6b848d87b
    
      * recovery: fix volmgr cleaning up
    
        Don't reset pointer to netlink manager
        Delete disks in stop() rather then in ~VolumeManager
        Call destroy() before deleting disks cause delete expects the
          disk to be destroyed
        Clear the lists or we would read garbage data on the next scan
    
        Change-Id: Idadfa1f33b7cb5f2f3c780848a99344a6608420e
    
      * recovery: handle interrupts in apply update menu
    
        Change-Id: I1f78f9196634353b77986545332d7d52a5f0c161
    
    Change-Id: Ic82d929e052b5ba70ecf7b475e0a223d77d9687e

---
+++ volume_manager/fs/Ntfs.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Ntfs.h"
+#include "Utils.h"
+
+#include <android-base/logging.h>
+#include <android-base/stringprintf.h>
+
+#include <string>
+#include <vector>
+
+#include <sys/mount.h>
+
+using android::base::StringPrintf;
+
+namespace android {
+namespace volmgr {
+namespace ntfs {
+
+static const char* kMountPath = "/sbin/mount.ntfs";
+
+status_t Mount(const std::string& source, const std::string& target, bool ro, bool remount,
+               bool executable, int ownerUid, int ownerGid, int permMask) {
+    char mountData[255];
+
+    const char* c_source = source.c_str();
+    const char* c_target = target.c_str();
+
+    sprintf(mountData,
+            "utf8,uid=%d,gid=%d,fmask=%o,dmask=%o,"
+            "shortname=mixed,nodev,nosuid,dirsync",
+            ownerUid, ownerGid, permMask, permMask);
+
+    if (!executable) strlcat(mountData, ",noexec", sizeof(mountData));
+    if (ro) strlcat(mountData, ",ro", sizeof(mountData));
+    if (remount) strlcat(mountData, ",remount", sizeof(mountData));
+
+    std::vector<std::string> cmd;
+    cmd.push_back(kMountPath);
+    cmd.push_back("-o");
+    cmd.push_back(mountData);
+    cmd.push_back(c_source);
+    cmd.push_back(c_target);
+
+    return ForkExecvp(cmd);
+}
+
+}  // namespace ntfs
+}  // namespace volmgr
+}  // namespace android
