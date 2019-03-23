5d5abd863e8758447eb7e68baff022df1332b3cf
Michael Bestas <mkbestas@lineageos.org>
2019-03-23 17:28:22 +0200

    recovery: Blank screen on init
    * Fixes touch on some devices like chiron/sagit
    
    * Some other devices (e.g. most MTK) need this because their kernel driver
      implementations do not perform as expected (initial unblank is only
      happening when altering the panel brightness x -> 0 -> y). In regular
      Android, that "unblanking" is done on boot when the lights HAL loads and
      sets the initial brightness.
    
    Change-Id: I2fffac508d09d07355a7a7f087805b0dceb5f97e

--- screen_ui.cpp
+++ screen_ui.cpp
@@ -873,6 +873,11 @@ bool ScreenRecoveryUI::Init(const std::string& locale) {
     return false;
   }
 
+#ifdef RECOVERY_UI_BLANK_UNBLANK_ON_INIT
+  gr_fb_blank(true);
+  gr_fb_blank(false);
+#endif
+
   // Are we portrait or landscape?
   layout_ = (gr_fb_width() > gr_fb_height()) ? LANDSCAPE : PORTRAIT;
   // Are we the large variant of our base layout?
