4cd3afa805f0074955f5923657c484013d3f82c9
Michael Bestas <mkbestas@lineageos.org>
2019-09-27 20:29:42 +0300

    recovery: Only show tests in eng builds
    Change-Id: If783711ecd0d20728ec27ebc572b57e6d9bee760

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -425,6 +425,11 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::ENTER_FASTBOOT);
   }
 
+  if (get_build_type() != "eng") {
+    device->RemoveMenuItemForAction(Device::RUN_GRAPHICS_TEST);
+    device->RemoveMenuItemForAction(Device::RUN_LOCALE_TEST);
+  }
+
   if (!is_ro_debuggable()) {
     device->RemoveMenuItemForAction(Device::ENTER_RESCUE);
   }
