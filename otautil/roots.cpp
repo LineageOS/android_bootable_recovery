166437c51b73b5921f22377973feb1f2ea13786b
Alessandro Astone <ales.astone@gmail.com>
2020-03-01 13:33:27 +0100

    recovery: make /etc/fstab only include entries that match the detected fs type
     * toybox's `mount` does not support multiple entries like we do,
       so if we can detect the filesystem of an fstab enrtry print that
       and only that one to /etc/fstab, so that mounting via toybox has
       a better chance of succeding.
    
     * as a bonus, this patch also gets rid of duplicates in /etc/fstab
       caused by the fact that ReadDefaultFstab() combines entries from
       DT and from recovery.fstab
    
    Change-Id: Iec4ab38044054555d2a33da6f5d53de7716e7bee

--- otautil/roots.cpp
+++ otautil/roots.cpp
@@ -67,29 +67,38 @@ void load_volume_table() {
     return;
   }
 
-  // Create a boring /etc/fstab so tools like Busybox work
-  FILE* file = fopen("/etc/fstab", "w");
-  if (!file) {
-    LOG(ERROR) << "Unable to create /etc/fstab";
-  }
-
   fstab.emplace_back(FstabEntry{
       .mount_point = "/tmp", .fs_type = "ramdisk", .blk_device = "ramdisk", .length = 0 });
 
+  Fstab fake_fstab;
   std::cout << "recovery filesystem table" << std::endl << "=========================" << std::endl;
   for (size_t i = 0; i < fstab.size(); ++i) {
     const auto& entry = fstab[i];
     std::cout << "  " << i << " " << entry.mount_point << " "
               << " " << entry.fs_type << " " << entry.blk_device << " " << entry.length
               << std::endl;
-    if (file) {
-      write_fstab_entry(entry, file);
+
+    if (std::find_if(fake_fstab.begin(), fake_fstab.end(), [entry](const FstabEntry& e) {
+          return entry.mount_point == e.mount_point;
+        }) == fake_fstab.end()) {
+      FstabEntry* entry_detectfs =
+          android::fs_mgr::GetEntryForMountPointTryDetectFs(&fstab, entry.mount_point);
+      if (entry_detectfs == &entry) {
+        fake_fstab.emplace_back(entry);
+      }
     }
   }
   std::cout << std::endl;
 
+  // Create a boring /etc/fstab so tools like Busybox work
+  FILE* file = fopen("/etc/fstab", "w");
   if (file) {
+    for (auto& entry : fake_fstab) {
+      write_fstab_entry(entry, file);
+    }
     fclose(file);
+  } else {
+    LOG(ERROR) << "Unable to create /etc/fstab";
   }
 }
 
