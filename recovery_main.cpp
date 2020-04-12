45aaaaf4a960a30650a42c795e91efe8379c0080
LuK1337 <priv.luk@gmail.com>
2020-04-12 19:04:59 +0200

    recovery: Add "Enable ADB" to advanced options
    Change-Id: I78ff93a43a17caeaae8a9601ec299b53a2802b20

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -458,9 +458,12 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::RUN_GRAPHICS_TEST);
     device->RemoveMenuItemForAction(Device::RUN_LOCALE_TEST);
     device->RemoveMenuItemForAction(Device::ENTER_RESCUE);
+  } else {
+    device->RemoveMenuItemForAction(Device::ENABLE_ADB);
   }
 
   if (get_build_type() == "user") {
+    device->RemoveMenuItemForAction(Device::ENABLE_ADB);
     device->RemoveMenuItemForAction(Device::WIPE_SYSTEM);
     device->RemoveMenuItemForAction(Device::MOUNT_SYSTEM);
   }
