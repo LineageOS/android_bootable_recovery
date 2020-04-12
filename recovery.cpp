5da659b274567134b9dbad8077c837d3aa6eb3c2
Nolen Johnson <johnsonnolen@gmail.com>
2020-04-12 14:13:31 -0400

    recovery_ui: Tell the user they're actually formatting
    * TWRP has given user the sentiment that "Wiping data",
      and "Factory Reset" both imply the exclusion of
      files in `/data/media/0` (/sdcard), whereas both AOSP
      (and Lineage Recovery by proxy) format the partitions.
    
    Change-Id: I36c5a59552d7d538a05762ca262e42dc78a1deac

--- recovery.cpp
+++ recovery.cpp
@@ -170,8 +170,8 @@ static bool yes_no(Device* device, const char* question1, const char* question2)
 }
 
 static bool ask_to_wipe_data(Device* device) {
-  std::vector<std::string> headers{ "Wipe all user data?", "  THIS CAN NOT BE UNDONE!" };
-  std::vector<std::string> items{ " Cancel", " Factory data reset" };
+  std::vector<std::string> headers{ "Format user data?", "This includes internal storage.", "THIS CANNOT BE UNDONE!" };
+  std::vector<std::string> items{ " Cancel", " Format data" };
 
   size_t chosen_item = device->GetUI()->ShowMenu(
       headers, items, 0, true,
@@ -458,7 +458,7 @@ change_menu:
       case Device::WIPE_CACHE: {
         save_current_log = true;
         std::function<bool()> confirm_func = [&device]() {
-          return yes_no(device, "Wipe cache?", "  THIS CAN NOT BE UNDONE!");
+          return yes_no(device, "Format cache?", "  THIS CAN NOT BE UNDONE!");
         };
         WipeCache(ui, ui->IsTextVisible() ? confirm_func : nullptr);
         if (!ui->IsTextVisible()) return Device::NO_ACTION;
@@ -468,7 +468,7 @@ change_menu:
       case Device::WIPE_SYSTEM: {
         save_current_log = true;
         std::function<bool()> confirm_func = [&device]() {
-          return yes_no(device, "Wipe system?", "  THIS CAN NOT BE UNDONE!");
+          return yes_no(device, "Format system?", "  THIS CAN NOT BE UNDONE!");
         };
         WipeSystem(ui, ui->IsTextVisible() ? confirm_func : nullptr);
         if (!ui->IsTextVisible()) return Device::NO_ACTION;
