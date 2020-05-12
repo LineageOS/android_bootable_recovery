6b86130061efebabc29e4e27d4bb344def84e13f
Richard Hansen <rhansen@rhansen.org>
2020-05-12 18:08:56 -0400

    recovery: Display recovery version
    Set the title lines to the lineage version and build date and render
    them between the logo and menu.
    
    The "Lineage Recovery" and "Android Fastboot" lines are removed
    because the logo already says "Recovery" or "Fastbootd".
    
    Also: Assume the logo and back icon are non-null.
    ScreenRecoveryUI::SelectMenu already makes this assumption, and it
    avoids questions about how to handle "back" if either the logo or
    the back icon is null.
    
    Change-Id: I63bb23e34e52fdb01dd85e793552aa259f46a7de

--- recovery_ui/screen_ui.cpp
+++ recovery_ui/screen_ui.cpp
@@ -810,33 +810,27 @@ void ScreenRecoveryUI::draw_menu_and_text_buffer_locked(
   int y = margin_height_;
 
   if (menu_) {
-    int x = margin_width_ + kMenuIndent;
-
-    SetColor(UIElement::INFO);
-
     auto& logo = fastbootd_logo_enabled_ ? fastbootd_logo_ : lineage_logo_;
-    if (logo && back_icon_) {
-      auto logo_width = gr_get_width(logo.get());
-      auto logo_height = gr_get_height(logo.get());
-      auto centered_x = ScreenWidth() / 2 - logo_width / 2;
-      DrawSurface(logo.get(), 0, 0, logo_width, logo_height, centered_x, y);
-      y += logo_height;
+    auto logo_width = gr_get_width(logo.get());
+    auto logo_height = gr_get_height(logo.get());
+    auto centered_x = ScreenWidth() / 2 - logo_width / 2;
+    DrawSurface(logo.get(), 0, 0, logo_width, logo_height, centered_x, y);
+    y += logo_height;
 
-      if (!menu_->IsMain()) {
-        auto icon_w = gr_get_width(back_icon_.get());
-        auto icon_h = gr_get_height(back_icon_.get());
-        auto icon_x = centered_x / 2 - icon_w / 2;
-        auto icon_y = y - logo_height / 2 - icon_h / 2;
-        gr_blit(back_icon_sel_ && menu_->selection() == -1 ? back_icon_sel_.get() : back_icon_.get(),
-                0, 0, icon_w, icon_h, icon_x, icon_y);
-      }
-      y += MenuItemPadding();
-    } else {
-      for (size_t i = 0; i < title_lines_.size(); i++) {
-        y += DrawTextLine(x, y, title_lines_[i], i == 0);
-      }
+    if (!menu_->IsMain()) {
+      auto icon_w = gr_get_width(back_icon_.get());
+      auto icon_h = gr_get_height(back_icon_.get());
+      auto icon_x = centered_x / 2 - icon_w / 2;
+      auto icon_y = y - logo_height / 2 - icon_h / 2;
+      gr_blit(back_icon_sel_ && menu_->selection() == -1 ? back_icon_sel_.get() : back_icon_.get(),
+              0, 0, icon_w, icon_h, icon_x, icon_y);
     }
 
+    int x = margin_width_ + kMenuIndent;
+    if (!title_lines_.empty()) {
+      SetColor(UIElement::INFO);
+      y += DrawTextLines(x, y, title_lines_);
+    }
     y += menu_->DrawHeader(x, y);
     menu_start_y_ = y + 12; // Skip horizontal rule and some margin
     menu_->SetMenuHeight(std::max(0, ScreenHeight() - menu_start_y_));
