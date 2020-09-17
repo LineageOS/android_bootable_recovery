d90300581292b2bcd792a756bd5f937304f2eb7c
Davide Garberi <dade.garberi@gmail.com>
2020-09-17 13:54:15 +0200

    Revert "Add a new key_pressed_mutex"
    This reverts commit b8a959b00fe57fc431034f9bea3c7b3380d4b6a5.
    
    Change-Id: I007b658b24c680a3c021bd0cf4ae6d1842c7a72b

--- recovery_ui/ui.cpp
+++ recovery_ui/ui.cpp
@@ -400,7 +400,7 @@ void RecoveryUI::ProcessKey(int key_code, int updown) {
   bool long_press = false;
 
   {
-    std::lock_guard<std::mutex> lg(key_press_mutex);
+    std::lock_guard<std::mutex> lg(key_queue_mutex);
     key_pressed[key_code] = updown;
     if (updown) {
       ++key_down_count;
@@ -444,7 +444,7 @@ void RecoveryUI::TimeKey(int key_code, int count) {
   std::this_thread::sleep_for(750ms);  // 750 ms == "long"
   bool long_press = false;
   {
-    std::lock_guard<std::mutex> lg(key_press_mutex);
+    std::lock_guard<std::mutex> lg(key_queue_mutex);
     if (key_last_down == key_code && key_down_count == count) {
       long_press = key_long_press = true;
     }
@@ -573,13 +573,13 @@ bool RecoveryUI::IsUsbConnected() {
 }
 
 bool RecoveryUI::IsKeyPressed(int key) {
-  std::lock_guard<std::mutex> lg(key_press_mutex);
+  std::lock_guard<std::mutex> lg(key_queue_mutex);
   int pressed = key_pressed[key];
   return pressed;
 }
 
 bool RecoveryUI::IsLongPress() {
-  std::lock_guard<std::mutex> lg(key_press_mutex);
+  std::lock_guard<std::mutex> lg(key_queue_mutex);
   bool result = key_long_press;
   return result;
 }
@@ -603,7 +603,7 @@ void RecoveryUI::FlushKeys() {
 
 RecoveryUI::KeyAction RecoveryUI::CheckKey(int key, bool is_long_press) {
   {
-    std::lock_guard<std::mutex> lg(key_press_mutex);
+    std::lock_guard<std::mutex> lg(key_queue_mutex);
     key_long_press = false;
   }
 
@@ -646,6 +646,6 @@ RecoveryUI::KeyAction RecoveryUI::CheckKey(int key, bool is_long_press) {
 void RecoveryUI::KeyLongPress(int) {}
 
 void RecoveryUI::SetEnableReboot(bool enabled) {
-  std::lock_guard<std::mutex> lg(key_press_mutex);
+  std::lock_guard<std::mutex> lg(key_queue_mutex);
   enable_reboot = enabled;
 }
