975d24a1ff549124f45ff2a1c76e6c3faa9a6ea0
Kevin F. Haggerty <haggertk@lineageos.org>
2022-04-07 12:09:47 -0600

    fixup! volume_manager: Move bind/mount failure from ERROR to WARN
    * The proper value is WARNING, not WARN
    
    Change-Id: I292875563a844fb8c12dd9dca49f983fb7eb3c3b

--- volume_manager/EmulatedVolume.cpp
+++ volume_manager/EmulatedVolume.cpp
@@ -77,7 +77,7 @@ status_t EmulatedVolume::doMount() {
         return -EIO;
     }
     if (BindMount(bindPath, getPath()) != OK) {
-        PLOG(WARN) << getId() << " failed to bind mount " << bindPath << " on " << getPath();
+        PLOG(WARNING) << getId() << " failed to bind mount " << bindPath << " on " << getPath();
         ForceUnmount(kStagingPath);
         return -EIO;
     }
