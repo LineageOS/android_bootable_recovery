15bc067373b4abe38a0afa4fe3d43effd75a10c7
pjgowtham <pjgowtham@gmail.com>
2022-04-08 13:39:58 +0530

    recovery: Extend A/B downgrades for older SPL
    This allows going back to stock ROM to progress
    
    Change-Id: If8a3ab7347e51c90337c4044826226ea11d6a327

--- recovery.cpp
+++ recovery.cpp
@@ -196,6 +196,19 @@ bool ask_to_continue_downgrade(Device* device) {
   }
 }
 
+bool ask_to_continue_spl_downgrade(Device* device) {
+  if (get_build_type() == "user") {
+    return false;
+  } else {
+    device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+    return yes_no(device,
+      "!!! WARNING !!! Downgrade detected. This can lead to permanent data loss
+      "THIS CANNOT BE UNDONE!"
+      "Restoring the system might require formatting the user data.",
+      "Install anyway?");
+  }
+}
+
 static bool ask_to_wipe_data(Device* device) {
   std::vector<std::string> headers{ "Format user data?", "This includes internal storage.", "THIS CANNOT BE UNDONE!" };
   std::vector<std::string> items{ " Cancel", " Format data" };
