d80fa1a95932f7fa2489107c1e08d1c7341378da
Michael Bestas <mkbestas@lineageos.org>
2022-05-11 06:39:40 +0300

    fastboot: Prefer ro.boot.hardware.revision for hw-revision
    ro.revision defaults to 0 if it's not set in cmdline.
    Some devices might want to set it in their device specific init files,
    however it's not possible to override ro properties.
    
    Test: Set ro.boot.hardware.revision in device specific init.rc and
          observe "HW version" in fastbootd UI
    
    Change-Id: I6e785c3fe3a49409e815af269a9a8db732b80ada

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -40,7 +40,8 @@ static const std::vector<std::pair<std::string, Device::BuiltinAction>> kFastboo
 void FillDefaultFastbootLines(std::vector<std::string>& title_lines) {
   std::string bootloader_version = android::base::GetProperty("ro.bootloader", "");
   std::string baseband_version = android::base::GetProperty("ro.build.expect.baseband", "");
-  std::string hw_version = android::base::GetProperty("ro.revision", "");
+  std::string hw_version = android::base::GetProperty(
+      "ro.boot.hardware.revision", android::base::GetProperty("ro.revision", ""));
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
   if (!android::base::EqualsIgnoreCase(bootloader_version, "unknown")) {
     title_lines.push_back("Bootloader version - " + bootloader_version);
