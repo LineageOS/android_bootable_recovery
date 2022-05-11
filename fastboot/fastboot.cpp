f7ec01ac86c33c9ac62f8236e44aacb0ed2afb07
Michael Bestas <mkbestas@lineageos.org>
2022-05-11 06:49:31 +0300

    fastboot: Hide bootloader/baseband versions if empty
    Change-Id: I557957da2a13a2c64b76f6126409363334af7a93

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -25,6 +25,7 @@
 
 #include <android-base/logging.h>
 #include <android-base/properties.h>
+#include <android-base/strings.h>
 #include <bootloader_message/bootloader_message.h>
 
 #include "recovery_ui/ui.h"
@@ -39,11 +40,16 @@ static const std::vector<std::pair<std::string, Device::BuiltinAction>> kFastboo
 Device::BuiltinAction StartFastboot(Device* device, const std::vector<std::string>& /* args */) {
   RecoveryUI* ui = device->GetUI();
 
+  std::string bootloader = android::base::GetProperty("ro.bootloader", "");
+  std::string baseband = android::base::GetProperty("ro.build.expect.baseband", "");
   std::vector<std::string> title_lines;
   title_lines.push_back("Product name - " + android::base::GetProperty("ro.product.device", ""));
-  title_lines.push_back("Bootloader version - " + android::base::GetProperty("ro.bootloader", ""));
-  title_lines.push_back("Baseband version - " +
-                        android::base::GetProperty("ro.build.expect.baseband", ""));
+  if (!android::base::EqualsIgnoreCase(bootloader, "unknown")) {
+    title_lines.push_back("Bootloader version - " + bootloader);
+  }
+  if (!baseband.empty()) {
+    title_lines.push_back("Baseband version - " + baseband);
+  }
   title_lines.push_back("Serial number - " + android::base::GetProperty("ro.serialno", ""));
   title_lines.push_back(std::string("Secure boot - ") +
                         ((android::base::GetProperty("ro.secure", "") == "1") ? "yes" : "no"));
