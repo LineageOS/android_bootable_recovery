bebc4ef755e0d84b086496895571f02fdc22e8f4
LuK1337 <priv.luk@gmail.com>
2023-07-29 11:16:41 +0200

    recovery: Ask to reboot before installing anything after A/B update
    Change-Id: Id3e44ddbe11119fcc66d379216f8f5e683979a9f

--- recovery.cpp
+++ recovery.cpp
@@ -171,6 +171,12 @@ static bool yes_no(Device* device, const char* question1, const char* question2)
   return (chosen_item == 1);
 }
 
+bool ask_to_ab_reboot(Device* device) {
+  device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+  return yes_no(device, "To install additional packages, you need to reboot recovery first",
+                "Do you want to reboot to recovery now?");
+}
+
 bool ask_to_continue_unverified(Device* device) {
   if (get_build_type() == "user") {
     return false;
