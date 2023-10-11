29bb9c3aac4fc37f4e5894f347338ab0bfcc0c8a
Davide Garberi <dade.garberi@gmail.com>
2023-10-11 11:02:52 +0200

    recovery: Allow flashing downgrades and unsigned builds on user
    Change-Id: I5ba9f14d4a984858872e07a0e42bd85493cbad31

--- recovery.cpp
+++ recovery.cpp
@@ -182,21 +182,13 @@ bool ask_to_ab_reboot(Device* device) {
 }
 
 bool ask_to_continue_unverified(Device* device) {
-  if (get_build_type() == "user") {
-    return false;
-  } else {
-    device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
-    return yes_no(device, "Signature verification failed", "Install anyway?");
-  }
+  device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+  return yes_no(device, "Signature verification failed", "Install anyway?");
 }
 
 bool ask_to_continue_downgrade(Device* device) {
-  if (get_build_type() == "user") {
-    return false;
-  } else {
-    device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
-    return yes_no(device, "This package will downgrade your system", "Install anyway?");
-  }
+  device->GetUI()->SetProgressType(RecoveryUI::EMPTY);
+  return yes_no(device, "This package will downgrade your system", "Install anyway?");
 }
 
 static bool ask_to_wipe_data(Device* device) {
