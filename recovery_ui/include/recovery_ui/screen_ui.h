c4d9c05a79578b8ec90cc27e4556eb4c930c5133
Chippa-a <bagirovvusal99@gmail.com>
2023-10-19 00:06:41 +0300

    recovery_ui: Add support for battery capacity status
    This is a reworked version from the older GUI
    implementation (I89c963e75bbc40f4bb7204211773fbfb28a5206b).
    
    - Update for the current recovery/fastbootd UI
    - Use loop to update battery capacity in background
    - Reflect charging status by appending "+"
    
    Change-Id: If7a6c080da273dfedddffcbea5e0ce12e677bf2f
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- recovery_ui/include/recovery_ui/screen_ui.h
+++ recovery_ui/include/recovery_ui/screen_ui.h
@@ -32,6 +32,7 @@
 class GRSurface;
 
 enum class UIElement {
+  BATTERY_LOW,
   HEADER,
   MENU,
   MENU_SEL_BG,
@@ -393,6 +394,7 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   virtual std::vector<std::string> GetMenuHelpMessage() const;
 
   virtual void draw_background_locked();
+  virtual void draw_battery_capacity_locked();
   virtual void draw_foreground_locked();
   virtual void draw_screen_locked();
   virtual void draw_menu_and_text_buffer_locked(const std::vector<std::string>& help_message);
@@ -402,6 +404,7 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   const GRSurface* GetCurrentFrame() const;
   const GRSurface* GetCurrentText() const;
 
+  void BattMonitorThreadLoop();
   void ProgressThreadLoop();
 
   virtual void ShowFile(FILE*);
@@ -523,6 +526,11 @@ class ScreenRecoveryUI : public RecoveryUI, public DrawInterface {
   bool rtl_locale_;
 
   std::mutex updateMutex;
+
+  std::thread batt_monitor_thread_;
+  std::atomic<bool> batt_monitor_thread_stopped_{ false };
+  int32_t batt_capacity_;
+  bool charging_;
 
   // Switch the display to active one after graphics is ready
   bool is_graphics_available;
