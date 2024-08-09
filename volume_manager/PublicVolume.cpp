62d5dfe8ac036119f16996f029102bb22d3966ca
Yumi Yukimura <me.cafebabe@gmail.com>
2024-08-10 03:43:17 +0800

    volume_manager: Add support for ISO9660/UDF CD-ROM
    This commit is squash of the following commits, with improvements:
    
    	Author: Yumi Yukimura <me.cafebabe@gmail.com>
    	Date:   Thu Jul 25 03:38:40 2024 +0800
    
    	    volume_manager: Handle USB CDROM that has both filesystem and partition table
    
    	    * Unlike SATA CDROM, USB CDROM has the same major and minor number,
    	      and the same block device name (sdX) as USB Disk.
    	    * For SATA CDROM, sgdisk_read() would fail, but for USB CDROM it
    	      won't fail if partition table is found.
    	    * ISO image generated by grub-mkrescue is in such format.
    
    	Author: Chih-Wei Huang <cwhuang@linux.org.tw>
    	Date:   Sat Mar 25 16:46:42 2017 +0000
    
    	    volume_manager: support UDF (Universal Disk Format)
    
    	    Refer to https://en.wikipedia.org/wiki/Universal_Disk_Format.
    
    	    [cafebabe: Apply fixups suggested by Luca Stefani]
    
    	Author: Chih-Wei Huang <cwhuang@linux.org.tw>
    	Date:   Sat Mar 25 16:45:41 2017 +0000
    
    	    volume_manager: auto mount CDROM
    
    	    Luo Chunbo's ISO9660 support (commit 133632d5) is not complete. Still
    	    need to handle block devices with major number of CDROM.
    
    	Author: Luo Chunbo <luochunbo@jidemail.com>
    	Date:   Wed May 25 16:16:48 2016 +0800
    
    	    volume_manager: ISO9660 support
    
    	    Ref: T7691
    
    	    [cafebabe: Remove MS_DIRSYNC and utf8 flags which are "Invalid argument", Refactor iso9660::Mount() (Suggested edit by Luca Stefani)]
    
    	    Signed-off-by: Luo Chunbo <luochunbo@jidemail.com>
    
    Change-Id: I92cedc0b23dfc98c874fddee189ec599c409b62f

--- volume_manager/PublicVolume.cpp
+++ volume_manager/PublicVolume.cpp
@@ -22,6 +22,7 @@
 #include "fs/Exfat.h"
 #include "fs/Ext4.h"
 #include "fs/F2fs.h"
+#include "fs/Iso9660.h"
 #include "fs/Ntfs.h"
 #include "fs/Vfat.h"
 
@@ -59,6 +60,11 @@ status_t PublicVolume::readMetadata() {
     status_t res = ReadMetadataUntrusted(mDevPath, mFsType, mFsUuid, label);
     if (!label.empty()) {
         setPartLabel(label);
+        // iso9660 has no UUID, we use label as UUID
+        if ((mFsType == "iso9660" || mFsType == "udf") && mFsUuid.empty() && !label.empty()) {
+            std::replace(label.begin(), label.end(), ' ', '_');
+            mFsUuid = label;
+        }
     }
     VolumeManager::Instance()->notifyEvent(ResponseCode::VolumeFsTypeChanged, mFsType);
     VolumeManager::Instance()->notifyEvent(ResponseCode::VolumeFsUuidChanged, mFsUuid);
@@ -104,6 +110,8 @@ status_t PublicVolume::doMount() {
         ret = ext4::Mount(mDevPath, getPath(), false, false, true, mMntOpts, false, true);
     } else if (mFsType == "f2fs") {
         ret = f2fs::Mount(mDevPath, getPath(), mMntOpts, false, true);
+    } else if (mFsType == "iso9660" || mFsType == "udf") {
+        ret = iso9660::Mount(mDevPath, getPath(), AID_MEDIA_RW, AID_MEDIA_RW);
     } else if (mFsType == "ntfs") {
         ret =
             ntfs::Mount(mDevPath, getPath(), false, false, false, AID_MEDIA_RW, AID_MEDIA_RW, 0007);
