e403de846bfc428914e58a5f2d5735ec0652f82b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-09-22 03:45:11 +0800

    recovery_ui: Make use of more keys on keyboard
    Change-Id: If7c0d0e55b9b0e8901dbb5ef9682adffa3550f89

--- recovery_ui/device.cpp
+++ recovery_ui/device.cpp
@@ -141,8 +141,10 @@ int Device::HandleMenuKey(int key, bool visible) {
     case BTN_DPAD_UP:
       return kHighlightUp;
 
+    case KEY_PAGEUP:
     case KEY_SCROLLUP:
       return kScrollUp;
+    case KEY_PAGEDOWN:
     case KEY_SCROLLDOWN:
       return kScrollDown;
 
@@ -156,10 +158,13 @@ int Device::HandleMenuKey(int key, bool visible) {
 
     case KEY_HOME:
     case KEY_HOMEPAGE:
+    case KEY_LEFTMETA:
+    case KEY_RIGHTMETA:
       return kGoHome;
 
     case KEY_BACKSPACE:
     case KEY_BACK:
+    case KEY_ESC:
       return kGoBack;
 
     case KEY_AGAIN:
