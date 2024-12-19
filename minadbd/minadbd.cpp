78abbad6001ad14db0535a62c73df937288252f1
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-19 21:41:28 +0800

    minadbd: Setup ADB over network (TCP/IP and VirtIO VSOCK)
    * Adds alternative way to use ADB sideload on devices without USB gadget mode
    * Code taken from android-15.0.0_r5 packages/modules/adb/daemon/main.cpp
    
    Change-Id: Ibef04214de79c991a863daef727979598050cc46

--- minadbd/minadbd.cpp
+++ minadbd/minadbd.cpp
@@ -23,15 +23,36 @@
 
 #include <android-base/logging.h>
 #include <android-base/parseint.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 
 #include "adb.h"
 #include "adb_auth.h"
+#include "daemon/transport_daemon.h"
 #include "transport.h"
 
 #include "minadbd/types.h"
 #include "minadbd_services.h"
 
 using namespace std::string_literals;
+
+static void minadbd_net_init() {
+  std::string prop_port = android::base::GetProperty("service.adb.tcp.port", "");
+
+  int port;
+  if (sscanf(prop_port.c_str(), "%d", &port) == 1 && port > 0) {
+    LOG(DEBUG) << "using tcp port=" << std::to_string(port);
+    // Listen on TCP and VSOCK port specified by service.adb.tcp.port property.
+    init_transport_socket_server(android::base::StringPrintf("tcp:%d", port));
+    init_transport_socket_server(android::base::StringPrintf("vsock:%d", port));
+  } else {
+    // Listen on default port.
+    init_transport_socket_server(
+        android::base::StringPrintf("tcp:%d", DEFAULT_ADB_LOCAL_TRANSPORT_PORT));
+    init_transport_socket_server(
+        android::base::StringPrintf("vsock:%d", DEFAULT_ADB_LOCAL_TRANSPORT_PORT));
+  }
+}
 
 int main(int argc, char** argv) {
   android::base::InitLogging(argv, &android::base::StderrLogger);
@@ -67,6 +88,8 @@ int main(int argc, char** argv) {
   socket_access_allowed = false;
 
   usb_init();
+
+  minadbd_net_init();
 
   VLOG(ADB) << "Event loop starting";
   fdevent_loop();
