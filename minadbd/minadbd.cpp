9a998297c5a2b12bae5ae0fb9b0653af69d867af
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-19 21:41:28 +0800

    minadbd: Setup ADB over network (TCP/IP and VirtIO VSOCK)
    * Adds alternative way to use ADB sideload on devices without USB gadget mode
    * Code taken from android-15.0.0_r5 packages/modules/adb/daemon/main.cpp
    
    Change-Id: Ibef04214de79c991a863daef727979598050cc46

--- minadbd/minadbd.cpp
+++ minadbd/minadbd.cpp
@@ -23,6 +23,8 @@
 
 #include <android-base/logging.h>
 #include <android-base/parseint.h>
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 
 #include "adb.h"
 #include "adb_auth.h"
@@ -32,6 +34,44 @@
 #include "minadbd_services.h"
 
 using namespace std::string_literals;
+
+static void setup_adb(const std::vector<std::string>& addrs) {
+  for (const auto& addr : addrs) {
+    LOG(INFO) << "adbd listening on " << addr;
+    local_init(addr);
+  }
+}
+
+static void minadbd_net_init() {
+  // If one of these properties is set, also listen on that port.
+  // If one of the properties isn't set and we couldn't listen on usb, listen
+  // on the default port.
+  std::vector<std::string> addrs;
+  std::string prop_addr = android::base::GetProperty("service.adb.listen_addrs", "");
+  if (prop_addr.empty()) {
+    std::string prop_port = android::base::GetProperty("service.adb.tcp.port", "");
+    if (prop_port.empty()) {
+      prop_port = android::base::GetProperty("persist.adb.tcp.port", "");
+    }
+
+    int port;
+    if (sscanf(prop_port.c_str(), "%d", &port) == 1 && port > 0) {
+      LOG(DEBUG) << "using tcp port=" << std::to_string(port);
+      // Listen on TCP and VSOCK port specified by service.adb.tcp.port property.
+      addrs.push_back(android::base::StringPrintf("tcp:%d", port));
+      addrs.push_back(android::base::StringPrintf("vsock:%d", port));
+      setup_adb(addrs);
+    } else {
+      // Listen on default port.
+      addrs.push_back(android::base::StringPrintf("tcp:%d", DEFAULT_ADB_LOCAL_TRANSPORT_PORT));
+      addrs.push_back(android::base::StringPrintf("vsock:%d", DEFAULT_ADB_LOCAL_TRANSPORT_PORT));
+      setup_adb(addrs);
+    }
+  } else {
+    addrs = android::base::Split(prop_addr, ",");
+    setup_adb(addrs);
+  }
+}
 
 int main(int argc, char** argv) {
   android::base::InitLogging(argv, &android::base::StderrLogger);
@@ -67,6 +107,8 @@ int main(int argc, char** argv) {
   socket_access_allowed = false;
 
   usb_init();
+
+  minadbd_net_init();
 
   VLOG(ADB) << "Event loop starting";
   fdevent_loop();
