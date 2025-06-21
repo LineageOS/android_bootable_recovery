9124ff26edb7742edaa2ff7ba8e7695feab3694d
LuK1337 <priv.luk@gmail.com>
2025-06-21 13:10:17 +0200

    recovery: Snapshot graphics_drm -> graphics_drm_qti
    cp graphics_drm{,_qti}.h
    cp graphics_drm{,_qti}.cpp
    
    sed -i "s/graphics_drm.h/graphics_drm_qti.h/g" graphics_drm_qti.*
    
    sed -i "s/GRSurfaceDrm/GRSurfaceDrmQti/g" graphics_drm_qti.*
    sed -i "s/GRSurfaceDrmQtis/GRSurfaceDrms/g" graphics_drm_qti.*
    
    sed -i "s/MinuiBackendDrm/MinuiBackendDrmQti/g" graphics_drm_qti.*
    
    clang-format --style=file -i graphics_drm_qti.*
    
    Change-Id: I710abd5505a120ff6fefbce9e66d5c29a1f24b79

---
+++ minui/graphics_drm_qti.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+
+#include <xf86drmMode.h>
+
+#include "graphics.h"
+#include "minui/minui.h"
+
+class GRSurfaceDrmQti : public GRSurface {
+ public:
+  ~GRSurfaceDrmQti() override;
+
+  // Creates a GRSurfaceDrmQti instance.
+  static std::unique_ptr<GRSurfaceDrmQti> Create(int drm_fd, int width, int height);
+
+  uint8_t* data() override {
+    return mmapped_buffer_;
+  }
+
+ private:
+  friend class MinuiBackendDrmQti;
+
+  GRSurfaceDrmQti(size_t width, size_t height, size_t row_bytes, size_t pixel_bytes, int drm_fd,
+                  uint32_t handle)
+      : GRSurface(width, height, row_bytes, pixel_bytes), drm_fd_(drm_fd), handle(handle) {}
+
+  const int drm_fd_;
+
+  uint32_t fb_id{ 0 };
+  uint32_t handle{ 0 };
+  uint8_t* mmapped_buffer_{ nullptr };
+};
+
+class MinuiBackendDrmQti : public MinuiBackend {
+ public:
+  MinuiBackendDrmQti() = default;
+  ~MinuiBackendDrmQti() override;
+
+  GRSurface* Init() override;
+  GRSurface* Flip() override;
+  void Blank(bool) override;
+  void Blank(bool blank, DrmConnector index) override;
+  bool HasMultipleConnectors() override;
+
+ private:
+  void DrmDisableCrtc(int drm_fd, drmModeCrtc* crtc);
+  bool DrmEnableCrtc(int drm_fd, drmModeCrtc* crtc, const std::unique_ptr<GRSurfaceDrmQti>& surface,
+                     uint32_t* conntcors);
+  void DisableNonMainCrtcs(int fd, drmModeRes* resources, drmModeCrtc* main_crtc);
+  bool FindAndSetMonitor(int fd, drmModeRes* resources);
+
+  struct DrmInterface {
+    std::unique_ptr<GRSurfaceDrmQti> GRSurfaceDrms[2];
+    int current_buffer{ 0 };
+    drmModeCrtc* monitor_crtc{ nullptr };
+    drmModeConnector* monitor_connector{ nullptr };
+    uint32_t selected_mode{ 0 };
+  } drm[DRM_MAX];
+
+  int drm_fd{ -1 };
+  DrmConnector active_display = DRM_MAIN;
+};
